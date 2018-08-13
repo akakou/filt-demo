40ec74971f64a292dd2dd86d3962b9138079c309
akakou <s16003@tokyo.kosen-ac.jp>
2018-08-14 05:08:26 +0900

    [add] 簡単なサーバ
    簡単なサーバを立てた

---
+++ demo/Assets/server.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Net;
+using System.Text;
+
+
+public class server : MonoBehaviour {
+	HttpListener listener;
+
+	// Use this for initialization
+	void Start () {	
+		Debug.Log("start !");
+			
+		System.Threading.Tasks.Task.Run( () => {			
+			InitServer();
+
+			while (true)
+			{
+				HttpListenerContext context = listener.GetContext();
+				HttpListenerResponse res = context.Response;
+				res.StatusCode = 200;
+
+				byte[] content = Encoding.UTF8.GetBytes("HELLO");
+				res.OutputStream.Write(content, 0, content.Length);
+
+				res.Close();
+				
+			}
+		});	
+
+		Debug.Log("end!");		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void InitServer() {
+		listener = new HttpListener();
+		listener.Prefixes.Add("http://localhost:8080/");
+		listener.Start();
+	}
+}
