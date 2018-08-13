8f20e0480d1c1abb067a61bb470388fdeec214ec
akakou <s16003@tokyo.kosen-ac.jp>
2018-08-13 23:35:42 +0900

    [add] 待つときのモーション
    待つときのモーションを2つにした

---
+++ demo/Assets/my_animation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class my_animation : MonoBehaviour {
+	private static int unit = 60;
+	private static int remain = 5 * unit;
+
+    private Animator animator;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (remain <= 0 && animator.GetInteger("wait") == 1) {
+            animator.SetInteger("wait", 0);
+			remain = 10 * unit;
+        }
+        if (remain <= 0 && animator.GetInteger("wait") == 0) {
+			animator.SetInteger("wait", 1);
+			remain = 5 * unit;
+        }
+
+		remain -= 1;
+	}
+}
